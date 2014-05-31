17c43571ce00c66f60ffd1169e1a2702c1a25c54
grelli_t <grelli_t@epitech.eu>
2014-05-31 14:39:34 +0200

    update

---
+++ conf_file/srcs/fill_angle.c
@@ -0,0 +1,17 @@
+
+#include "raytracer.h"
+
+void    fill_sphere_angle(t_params *obj, char *line)
+{
+  int   c;
+
+  c = my_strlen(line) - 1;
+  if (line[c] == ';')
+    line[c] = '\0';
+  my_putstr("\n------------------");
+  my_putstr(line);
+  my_putstr("------------------\n");
+  if (obj->objs->ray = atof(line) <= 0)
+    obj->objs->ray = 50;
+  printf("LOL<>LOL>LOL>LOL> = %f\n", obj->objs->ray);
+}
