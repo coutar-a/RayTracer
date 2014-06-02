51d72b0339196e36af351e4dbac229403fb33578
grelli_t <grelli_t@epitech.eu>
2014-06-02 11:45:20 +0200

    ca marche presque

--- src/conf/fill_objects.c
+++ src/conf/fill_objects.c
@@ -5,7 +5,7 @@
 ** Login   <grelli_t@epitech.net>
 **
 ** Started on  Tue May 27 11:50:51 2014 grelli_t
-** Last update Mon Jun  2 10:41:31 2014 grelli_t
+** Last update Mon Jun  2 11:39:56 2014 grelli_t
 */
 
 #include <stdlib.h>
@@ -41,7 +41,7 @@ int		fill_diffrent_object(t_params *params, char **file, int *i,
   else if (flag == 2)
     params->objs[j].type = CONE;
   else if (flag == 3)
-    params->objs[j].type = CYLINDER;
+    params->objs[j].type = 4;
   else if (flag == 4)
     params->objs[j].type = PLAN;
   if (fill_object(params, file, i, j) == ERROR)
